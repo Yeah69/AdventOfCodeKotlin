c3040dc3c71d39b8bd3eac09d5e34360d814f46c
Dima Enns <dieter.enns@gmail.com>
2020-12-01 06:51:58 +0100

    Solved day one

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,4 +1,33 @@
 class Day01 : Day {
-    override fun taskZeroLogic(input: String): String = "Uh yeah!"
-    override fun taskOneLogic(input: String): String = "asdf"
+    private val seekedSum = 2020
+
+    override fun taskZeroLogic(input: String): String {
+        val expenses = getExpenses(input)
+
+        for (i in expenses.indices) {
+            for (j in expenses.indices) {
+                if (i != j
+                    && expenses[i] + expenses[j] == seekedSum) {
+                    return (expenses[i] * expenses[j]).toString();
+                }
+            }
+        }
+        return "No solution found"
+    }
+    override fun taskOneLogic(input: String): String {
+        val expenses = getExpenses(input)
+
+        for (i in expenses.indices) {
+            for (j in expenses.indices) {
+                for (k in expenses.indices) {
+                    if (i != j && i !=k && j != k
+                        && expenses[i] + expenses[j] + expenses[k] == seekedSum) {
+                        return (expenses[i] * expenses[j] * expenses[k]).toString();
+                    }
+                }
+            }
+        }
+        return "No solution found"
+    }
+    private fun getExpenses(input: String): List<Int> = input.split('\n').map { it.toInt() }.toList()
 }
