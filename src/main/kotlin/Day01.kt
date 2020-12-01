a8ecd6734009db5ee8dc5d232ae02b0bebe8e68e
Dima Enns <dieter.enns@gmail.com>
2020-12-01 07:31:43 +0100

    Playing around with language features like extension methods

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -2,20 +2,13 @@ class Day01 : Day {
     private val seekedSum = 2020
 
     override fun taskZeroLogic(input: String): String {
-        val expenses = getExpenses(input)
-
-        for (i in expenses.indices) {
-            for (j in expenses.indices) {
-                if (i != j
-                    && expenses[i] + expenses[j] == seekedSum) {
-                    return (expenses[i] * expenses[j]).toString();
-                }
-            }
-        }
-        return "No solution found"
+        val maybePair = input.parseExpenses().allPairs().firstOrNull { pair -> pair.first + pair.second == seekedSum }
+        return if (maybePair is Pair<Int,Int>)
+                (maybePair.first * maybePair.second).toString()
+        else "no solution found"
     }
     override fun taskOneLogic(input: String): String {
-        val expenses = getExpenses(input)
+        val expenses = input.parseExpenses()
 
         for (i in expenses.indices) {
             for (j in expenses.indices) {
@@ -29,5 +22,17 @@ class Day01 : Day {
         }
         return "No solution found"
     }
-    private fun getExpenses(input: String): List<Int> = input.split('\n').map { it.toInt() }.toList()
+    private fun String.parseExpenses(): List<Int> = this.split('\n').map { it.toInt() }.toList()
+    private fun <T> List<T>.allPairs(): Sequence<Pair<T, T>> {
+        val list = this
+        return sequence {
+            val iterated = mutableListOf<T>()
+            for (i: T in list) {
+                for (j: T in iterated) {
+                    yield(Pair(i, j))
+                }
+                iterated.add(i)
+            }
+        }
+    }
 }
