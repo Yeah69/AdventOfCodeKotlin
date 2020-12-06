ff8dd1ce3f786ae7a0ada5999767645e77e0e0d7
Dima Enns <dieter.enns@gmail.com>
2020-12-06 07:35:30 +0100

    day 6

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,11 +1,19 @@
+import kotlin.streams.asSequence
+
 class Day06 : Day() {
     override val label: String
         get() = "06"
 
-    override fun taskZeroLogic(): String {
-        return "no solution found"
-    }
-    override fun taskOneLogic(): String {
-        return "no solution found"
-    }
+    private val groups by lazy { input
+        .split("\r\n\r\n")
+        .asSequence()
+        .map { it.lineSequence().map { line -> line.chars().asSequence().toList() }
+        .toList() } }
+
+    private fun logic(countVotes: (Sequence<Int>, Int) -> Int): String =
+        groups.map { countVotes(it.asSequence().flatten(), it.count()) }.sum().toString()
+
+    override fun taskZeroLogic(): String = logic { s, _ -> s.distinct().count() }
+
+    override fun taskOneLogic(): String = logic { s, c -> s.groupBy { i -> i }.values.count { g -> g.count() == c } }
 }
