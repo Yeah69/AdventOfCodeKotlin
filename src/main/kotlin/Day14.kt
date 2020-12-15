3714135ffc929df0db3cf95d0ddd7a45a1dba493
Dima Enns <dieter.enns@gmail.com>
2020-12-15 10:04:25 +0100

    day 14 improvements

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,23 +1,14 @@
 import java.lang.Exception
 
-fun String.toMaskValue(): ULong = this
-    .replace("_", "")
-    .reversed()
-    .asSequence()
-    .mapIndexed { i, c -> Pair(i, c) }
-    .filter { p -> p.second == '1' }
-    .map { p -> 1uL.shl(p.first) }
-    .sum()
-
 class Day14 : Day() {
     override val label: String get() = "14"
 
     interface Instruction {
-        fun execute(mask: Mask, map: MutableMap<ULong, ULong>): Mask
+        fun execute(mask: Mask, map: MutableMap<Long, Long>): Mask
     }
 
-    abstract class Mask(val mask: String) : Instruction {
-        override fun execute(mask: Mask, map: MutableMap<ULong, ULong>): Mask = this
+    abstract class Mask(private val mask: String) : Instruction {
+        override fun execute(mask: Mask, map: MutableMap<Long, Long>): Mask = this
 
         abstract fun transform(value: String): String
 
@@ -25,7 +16,7 @@ class Day14 : Day() {
             mask.zip(value).map(map).joinToString("")
     }
 
-    class MaskZero(mask1: String) : Mask(mask1) {
+    class MaskZero(maskParam: String) : Mask(maskParam) {
         override fun transform(value: String): String = transformInner(value) {
             (c1, c2) -> when (c1) {
                 '0' -> '0'
@@ -33,7 +24,7 @@ class Day14 : Day() {
                 else -> c2 } }
     }
 
-    class MaskOne(mask1: String) : Mask(mask1) {
+    class MaskOne(maskParam: String) : Mask(maskParam) {
         override fun transform(value: String): String = transformInner(value) {
             (c1, c2) -> when (c1) {
                 '0' -> c2
@@ -41,24 +32,34 @@ class Day14 : Day() {
                 else -> 'X' } }
     }
 
-    interface MemoryWrite : Instruction
+    abstract class MemoryWrite : Instruction {
+        fun String.toMaskValue(): Long = this
+            .replace("_", "")
+            .reversed()
+            .asSequence()
+            .mapIndexed { i, c -> Pair(i, c) }
+            .filter { p -> p.second == '1' }
+            .map { p -> 1L.shl(p.first) }
+            .sum()
+    }
 
-    data class MemoryWriteZero(val address: String, val value: String) : MemoryWrite {
-        override fun execute(mask: Mask, map: MutableMap<ULong, ULong>): Mask {
+    data class MemoryWriteZero(val address: String, val value: String) : MemoryWrite() {
+        override fun execute(mask: Mask, map: MutableMap<Long, Long>): Mask {
             map[address.toMaskValue()] = mask.transform(value).toMaskValue()
             return mask
         }
     }
 
-    data class MemoryWriteOne(val address: String, val value: String) : MemoryWrite {
+    data class MemoryWriteOne(val address: String, val value: String) : MemoryWrite() {
 
         private fun recursive(text: String) : Sequence<String> =
             if (text.contains('X').not())
                 sequenceOf(text)
             else
-                recursive(text.replaceFirst('X', '0')) + recursive(text.replaceFirst('X', '1'))
+                (recursive(text.replaceFirst('X', '0'))
+                        + recursive(text.replaceFirst('X', '1')))
 
-        override fun execute(mask: Mask, map: MutableMap<ULong, ULong>): Mask {
+        override fun execute(mask: Mask, map: MutableMap<Long, Long>): Mask {
             recursive(mask.transform(address)).forEach { map[it.toMaskValue()] = value.toMaskValue() }
             return mask
         }
@@ -67,9 +68,9 @@ class Day14 : Day() {
     class Program(private val instructions: List<Instruction>) {
         private var mask: Mask = MaskZero("111111111111111111111111111111111111")
 
-        val map = mutableMapOf<ULong, ULong>()
+        val map = mutableMapOf<Long, Long>()
 
-        fun run(): ULong {
+        fun run(): Long {
             instructions.forEach { mask = it.execute(mask, map) }
             return map.values.sum()
         }
@@ -78,6 +79,11 @@ class Day14 : Day() {
     private val maskRegex: Regex = """^mask = ([01X]{36})$""".toRegex()
 
     private val memRegex: Regex = """^mem\[([0-9]+)] = ([0-9]+)$""".toRegex()
+
+    private fun Long.toBinaryText() = (35 downTo 0)
+        .asSequence()
+        .map { if((1L.shl(it).and(this).shr(it)) == 1L) '1' else '0' }
+        .joinToString("")
 
     private fun getInstruction(
         createMemoryWrite: (String, String) -> MemoryWrite,
@@ -88,27 +94,15 @@ class Day14 : Day() {
                 val memMatch = memRegex.matchEntire(it)
                 if (memMatch != null) {
                     val (address, value) = memMatch.destructured
-                    val uLValue = value.toULong()
-                    val textValue = (35 downTo 0)
-                        .asSequence()
-                        .map { if((1uL.shl(it).and(uLValue).shr(it)) == 1uL) '1' else '0' }
-                        .joinToString("")
-                    val uLaddressValue = address.toULong()
-                    val textaddressValue = (35 downTo 0)
-                        .asSequence()
-                        .map { if((1uL.shl(it).and(uLaddressValue).shr(it)) == 1uL) '1' else '0' }
-                        .joinToString("")
-                    createMemoryWrite(textaddressValue, textValue) }
+                    createMemoryWrite(address.toLong().toBinaryText(), value.toLong().toBinaryText()) }
                 else {
                     val (mask) = maskRegex.find(it)?.destructured ?: throw Exception()
                     createMask(mask) } }
             .toList()
 
-    private val programZero: Program
-        get() =  Program(getInstruction(::MemoryWriteZero, ::MaskZero))
+    private val programZero by lazy { Program(getInstruction(::MemoryWriteZero, ::MaskZero)) }
 
-    private val programOne: Program
-        get() =  Program(getInstruction(::MemoryWriteOne, ::MaskOne))
+    private val programOne by lazy { Program(getInstruction(::MemoryWriteOne, ::MaskOne)) }
 
     override fun taskZeroLogic(): String = programZero.run().toString()
 
