74d6b07d045dcc94664c5e0f18fd5b56ab7352fa
Dima Enns <dieter.enns@gmail.com>
2020-12-22 12:52:42 +0100

    day 22

--- src/test/kotlin/Day22Test.kt
+++ src/test/kotlin/Day22Test.kt
@@ -4,7 +4,7 @@ import kotlin.test.assertEquals
 class Day22Test {
     private val day by lazy { Day22() }
     @Test
-    fun zero() = assertEquals(noSolutionFound, day.taskZeroLogic())
+    fun zero() = assertEquals("32179", day.taskZeroLogic())
     @Test
-    fun one() = assertEquals(noSolutionFound, day.taskOneLogic())
+    fun one() = assertEquals("30498", day.taskOneLogic())
 }
